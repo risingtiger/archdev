9a39af46561b054231c83e6583180aa7fa6b829b
Davis Hammon <accounts@risingtiger.com>
2022-06-05 17:39:26 -0600

    Update brewinstalls.sh

--- macosinit/brewinstalls.sh
+++ macosinit/brewinstalls.sh
@@ -3,10 +3,12 @@
 
 cd ~/Desktop
 
+brew install bat
 brew install broot
 brew install ccls
 brew install cmake
 brew install dfu-util
+brew install exa
 brew install fd
 brew install fzf
 brew install git
@@ -15,7 +17,7 @@ brew install llvm
 brew install neovim
 brew install nnn
 brew install node@17
-brew install platformio
+#brew install platformio
 brew install rename
 brew install ripgrep
 brew install rsync
@@ -23,5 +25,5 @@ brew install rustup
 rust install rust-analyzer
 brew install sd
 brew install tree-sitter
-brew install w3m
+#brew install w3m
 
